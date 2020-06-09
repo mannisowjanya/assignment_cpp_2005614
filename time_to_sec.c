a88998972b442fd7bedb8c7880138c379bf5e936
Sowjanya_2005614 <55553331+mannisowjanya@users.noreply.github.com>
2020-06-09 11:37:56 +0530

    Update time_to_sec.c

--- time_to_sec.c
+++ time_to_sec.c
@@ -3,9 +3,9 @@ int main()
 {
 
     int hh,mm,ss,hour,min;
-    printf("hh:mm:ss\n");
-    scanf("%d:%d:%d",&hh,&mm,&ss);
-     hour=(hh*3600);
+    print("hh:mm:ss\n");
+    scan("%d:%d:d",&hh,mm,&ss);
+     hour=(h*3600);
      min=mm*60;
      printf("total seconds = %d\n",(hour+min+ss));
      return 0;
